cdef0193518a4682f39dad72e77ee948ff0c61a6
xidios <47945350+xidios@users.noreply.github.com>
2021-05-16 20:59:07 +0700

    Добавьте файлы проекта.

---
+++ lab14/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace lab14
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(70, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(70, 11);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(70, 41);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown2.TabIndex = 2;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(273, 11);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Mean";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Variance";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(227, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Count";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(227, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "AvgError%";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(227, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "VarianceError%";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(226, 107);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Squared";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(319, 58);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 15);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "AvgError%";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(319, 83);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 15);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "AvgError%";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(319, 107);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(63, 15);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "AvgError%";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(319, 134);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(63, 15);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "AvgError%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 158);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ lab14/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab14
+{
+    public partial class Form1 : Form
+    {
+        public List<double> values;
+        public List<double> stats;
+
+
+        double theory_chi = 11.070; 
+        double mean;
+        double variable;
+        int N = 1;
+        int m; // Count of intervals
+        Random Rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            numericUpDown1.DecimalPlaces = 2;
+            numericUpDown2.DecimalPlaces = 2;
+        }
+
+        double P(double x)
+        {
+            return Math.Pow(Math.E, -1.0 * (x - mean) * (x - mean) / (2.0 * variable)) / (Math.Sqrt(2 * Math.PI * variable));
+        }
+
+        void ProcessData()
+        {
+
+
+
+            N = (int)numericUpDown3.Value;
+
+            mean = (double)numericUpDown1.Value;
+
+            variable = (double)numericUpDown2.Value;
+            m = (int)Math.Sqrt(N) + 1;
+
+            values = new List<double>();
+
+            double EmpM = 0;
+            double EmpD = 0;
+
+            double rvs;
+            for (int i = 0; i < N; i++)
+            {
+                rvs = GeneratePsi();
+                values.Add(rvs);
+                EmpM += rvs;
+                EmpD += rvs * rvs;
+            }
+
+
+
+            EmpM /= N;
+            EmpD = EmpD / N - EmpM * EmpM;
+            double minValue = values.Min();
+            double maxValue = values.Max();
+
+            stats = new List<double>();
+            stats.AddRange(Enumerable.Repeat(0d, m));
+
+            int k = (int)Math.Log(N, 2) + 1;
+
+            for (int i = 0; i < N; i++)
+            {
+                double cur = values[i];
+                int j = 0;
+                while (cur >= minValue)
+                {
+                    cur -= (double)(maxValue - minValue) / m;
+                    j++;
+                }
+                if (j > k)
+                    j = k; 
+                stats[j - 1] += (double)(1.0 / N);
+            }
+
+            double A = minValue;
+            double step = (double)(maxValue - minValue) / m;
+            double Chi_part0 = 0;
+
+            for (int i = 0; i < m; i++)
+            {
+                Chi_part0 += stats[i] * stats[i] / (double)(N * step * P((A + step) / 2.0));
+                A += step;
+            }
+
+            double Chi_squared_test = Chi_part0 - N;
+
+            CountStats(EmpM, EmpD, Chi_squared_test);
+
+        }
+
+
+        double CountAbsoluteMeanError(double mean, double EmpM)
+        {
+            return Math.Abs(EmpM - mean);
+
+        }
+
+        double CountAbsoluteVarError(double DVar, double EmpVar)
+        {
+            return Math.Abs(EmpVar - DVar);
+        }
+
+        double CountRelativeMeanError(double mean, double EmpM)
+        {
+            return CountAbsoluteMeanError(mean, EmpM) / Math.Abs(mean);
+        }
+
+        double CountRelativeVarError(double DVar, double EmpVar)
+        {
+            return CountAbsoluteVarError(DVar, EmpVar) / Math.Abs(DVar);
+        }
+
+        void CountStats(double EmpMean, double EmpVar, double chiSquaredTest)
+        {
+            double rme = CountRelativeMeanError(mean, EmpMean) * 100;
+            double rve = CountRelativeVarError(variable, EmpVar) * 100;
+            int rme_i = (int)rme;
+            int rve_i = (int)rve;
+
+            label7.Text = EmpMean.ToString() + "(" + rme_i.ToString() + "%)";
+            label8.Text = EmpVar.ToString() + "(" + rve_i.ToString() + "%)";
+
+            label9.Text = chiSquaredTest.ToString() + " > " + theory_chi.ToString();
+            bool result = !(chiSquaredTest > theory_chi);
+            label10.Text = result.ToString();
+        }
+
+        double GeneratePsi()
+        {
+            double psi = Math.Sqrt(-2.0 * Math.Log(Rand.NextDouble())) * Math.Cos(2.0 * Math.PI * Rand.NextDouble());
+
+            psi = psi * Math.Sqrt(variable) + mean;
+            return psi;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProcessData();
+        }
+    }
+}
